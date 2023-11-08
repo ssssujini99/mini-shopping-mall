98b59bda4880403113ce695735e954dfee12a70e
수지니 <sssssjin99@naver.com>
2023-11-08 23:06:54 +0900

    [settings]: 첫 프로젝트 세팅

---
+++ src/main/java/com/programmers/zigzag/ZigzagApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.zigzag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZigzagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZigzagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programmers/zigzag/ZigzagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.zigzag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZigzagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
