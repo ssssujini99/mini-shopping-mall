fe9941505624bc0d4cf0a928939fe15b03b7fc30
수지니 <sssssjin99@naver.com>
2023-11-12 04:56:18 +0900

    [feat]: ProductControllerAdvice 추가

---
+++ src/main/java/com/programmers/zigzag/common/advice/ProductControllerAdvice.java
@@ -0,0 +1,17 @@
+package com.programmers.zigzag.common.advice;
+
+import com.programmers.zigzag.product.exception.ProductStockQuantityInvalidException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ProductControllerAdvice {
+
+    @ExceptionHandler(ProductStockQuantityInvalidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ErrorTemplate handleProductStockQuantityInvalidException(RuntimeException runtimeException) {
+        return ErrorTemplate.of(runtimeException.getMessage());
+    }
+}
