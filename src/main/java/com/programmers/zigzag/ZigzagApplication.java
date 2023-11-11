db43038ff7e3c6bb19d582b1eb2608fce9c48dfb
수지니 <sssssjin99@naver.com>
2023-11-11 22:22:03 +0900

    [feat]: @EnableJpaAuditing 추가

--- src/main/java/com/programmers/zigzag/ZigzagApplication.java
+++ src/main/java/com/programmers/zigzag/ZigzagApplication.java
@@ -2,7 +2,9 @@ package com.programmers.zigzag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ZigzagApplication {
 
