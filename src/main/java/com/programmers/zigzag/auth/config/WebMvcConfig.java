47466f1bb56d8290ea7cfeeb9bf0ad2e8d4c37fc
수지니 <sssssjin99@naver.com>
2023-11-11 21:51:41 +0900

    [feat]: WebMvcConfig 추가

---
+++ src/main/java/com/programmers/zigzag/auth/config/WebMvcConfig.java
@@ -0,0 +1,31 @@
+package com.programmers.zigzag.auth.config;
+
+import com.programmers.zigzag.auth.interceptor.AuthorityInterceptor;
+import com.programmers.zigzag.auth.interceptor.JwtTokenInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final JwtTokenInterceptor jwtTokenInterceptor;
+    private final AuthorityInterceptor authorityInterceptor;
+
+    private static final String[] JWT_INTERCEPTOR_URLS = {
+            "/auth/info",
+    };
+
+    private static final String[] AUTH_INTERCEPTOR_URLS = {
+    };
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtTokenInterceptor).addPathPatterns(JWT_INTERCEPTOR_URLS);
+        registry.addInterceptor(authorityInterceptor).addPathPatterns(AUTH_INTERCEPTOR_URLS);
+    }
+
+
+}
